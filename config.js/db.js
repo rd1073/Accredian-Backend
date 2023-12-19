8de52efe33aed6aea58c999ba6a7d426b257e0c0
RD <104590657+rd1073@users.noreply.github.com>
2023-12-19 14:07:59 +0530

    Update db.js

--- config.js/db.js
+++ config.js/db.js
@@ -2,7 +2,7 @@ const { Sequelize } =require("sequelize");
  
 const db = new Sequelize('auth', 'root', '', {
     host: "127.0.0.1",
-    password:"Golobull123#",
+    password:"***",
     dialect: "mysql",
     database: "Auth",
 });
